210708591391b01e48ea76b24e361edf4b4dc74d
cseDeveloper06 <faridayasmeencse@gmail.com>
2023-09-27 15:52:49 +0600

    added Donation Details page

--- src/components/DonationDetails/DonationDetails.jsx
+++ src/components/DonationDetails/DonationDetails.jsx
@@ -2,6 +2,7 @@ import { useLoaderData, useParams } from "react-router-dom";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { saveDonationApplication } from "../../utility/localStorage";
+import './Donate.css'
 
 
 
@@ -11,7 +12,7 @@ const DonationDetails = () => {
     const {id} = useParams();
     const idInt = parseInt(id);
     const donation = donations.find(donation => donation.id === idInt);
-    const {image,price,title,description} = donation;
+    const {image,price,title,description,text_color} = donation;
 
     const handleApplyDonate = () =>{
         saveDonationApplication (idInt);
@@ -20,15 +21,21 @@ const DonationDetails = () => {
 
     return (
         <div>
-            <div className="p-8">
-                <img className="h-[550px] w-[1100px] ml-20" src={image} alt="" />
+            <div>
+                <div className="p-8 relative">
+                    <img className="h-[550px] w-[1100px] ml-20" src={image} alt="" />
+                        <div className="absolute donate-bg h-32 w-[1100] -mt-32 right-[136px] left-[110px]">
+                             <button onClick={handleApplyDonate} 
+                            className="btn ml-10 mt-10 text-white border-0" 
+                            style={{backgroundColor:text_color}}>Donate<span>{price}TK</span></button>
+                        </div>
+                </div>
+               
             </div>
             <div>
-                <button onClick={handleApplyDonate} className="btn">Donate<span>{price}TK</span></button>
+                <h1 className="text-4xl font-bold p-6">{title}</h1>
+                <p className="text-[16] text-[hsl(0,0%,64%)] text-justify px-6 pb-8">{description}</p>
             </div>
-            <h1>{title}</h1>
-            <p>{description}</p>
-
             <ToastContainer />
         </div>
     );
